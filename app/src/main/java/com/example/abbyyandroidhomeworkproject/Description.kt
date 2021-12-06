46551408c4451bc844dc48e17663f08eb9b15266
boulygin233 <boulygin.artem@gmail.com>
2021-12-06 13:30:37 +0300

    CardView and ANimation homework

---
+++ app/src/main/java/com/example/abbyyandroidhomeworkproject/Description.kt
@@ -0,0 +1,12 @@
+package com.example.abbyyandroidhomeworkproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Description : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTitle(R.string.hzd_name)
+        setContentView(R.layout.activity_description)
+    }
+}
---
+++ app/src/main/java/com/example/abbyyandroidhomeworkproject/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.abbyyandroidhomeworkproject
+
+import android.animation.Animator
+import android.animation.AnimatorInflater
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.core.animation.addListener
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun getDescription(view: View) {
+        val intent = Intent(this, Description::class.java)
+        val card:View = findViewById(R.id.hzd_card)
+        val animator: Animator = AnimatorInflater.loadAnimator(this, R.animator.card_animation)
+            .apply {
+                setTarget(card)
+                start()
+            }
+        animator.addListener({startActivity(intent)})
+
+    }
+}
